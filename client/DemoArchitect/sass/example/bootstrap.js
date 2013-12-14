15cb9471f1bdaadfc6515911e22459ef2eacad45
jurisv <juris@extjs.com>
2013-12-13 17:38:40 -0800

    Update Architect example to production setting

--- client/DemoArchitect/sass/example/bootstrap.js
+++ client/DemoArchitect/sass/example/bootstrap.js
@@ -439,7 +439,6 @@ Ext.ClassManager.addNameAlternateMappings({
   ],
   "Ext.grid.RowEditor": [],
   "Ext.grid.RowEditorButtons": [],
-  "Ext.grid.Scroller": [],
   "Ext.grid.View": [],
   "Ext.grid.ViewDropZone": [],
   "Ext.grid.column.Action": [
@@ -1325,7 +1324,6 @@ Ext.ClassManager.addNameAliasMappings({
   "Ext.grid.RowEditorButtons": [
     "widget.roweditorbuttons"
   ],
-  "Ext.grid.Scroller": [],
   "Ext.grid.View": [
     "widget.gridview"
   ],
