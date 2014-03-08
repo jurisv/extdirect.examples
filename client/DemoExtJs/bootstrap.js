b45051e59e3a04cd357428c7f03cdac0f8b7c0c9
jurisv <juris@extjs.com>
2014-03-08 13:15:28 -0800

    Update ExtJs and Sencha Touch projects to consume latest CMD 4.0.2.67

--- client/DemoExtJs/bootstrap.js
+++ client/DemoExtJs/bootstrap.js
@@ -1688,7 +1688,7 @@ Ext.setVersion("ext-theme-neutral", "4.2.1");
  * @author Jacky Nguyen <jacky@sencha.com>
  */
 (function() {
-    var head = document.head;
+    var head = document.head || document.getElementsByTagName('head')[0];
 
     function write(content) {
         document.write(content);
@@ -1841,9 +1841,13 @@ Ext.setVersion("ext-theme-neutral", "4.2.1");
             if (!filterPlatform(platform) || filterPlatform(exclude)) {
                 continue;
             }
-            Ext.theme = {
-                name: theme || 'Default'
-            };
+
+            if(!Ext.theme) {
+                Ext.theme = {};
+            }
+            if(!Ext.theme.name) {
+                Ext.theme.name = theme || 'Default';
+            }
         }
 
         write('<link rel="stylesheet" href="'+path+'">');
