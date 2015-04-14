53ed15e57a557e96588a3ea9741fa91045075563
jurisv <juris@extjs.com>
2015-04-14 02:56:52 -0700

    WIP. Scaffold Touch app

---
+++ client/touch/app.js
@@ -0,0 +1,31 @@
+/*
+    This file is generated and updated by Sencha Cmd. You can edit this file as
+    needed for your application, but these edits will have to be merged by
+    Sencha Cmd when it performs code generation tasks such as generating new
+    models, controllers or views and when running "sencha app upgrade".
+
+    Ideally changes to this file would be limited and most work would be done
+    in other places (such as Controllers). If Sencha Cmd cannot merge your
+    changes and its generated code, it will produce a "merge conflict" that you
+    will need to resolve manually.
+*/
+
+Ext.application({
+    name: 'Demo',
+
+    requires: [
+        'Ext.MessageBox'
+    ],
+
+    views: [
+        'Main'
+    ],
+
+    launch: function() {
+        // Destroy the #appLoadingIndicator element
+        Ext.fly('appLoadingIndicator').destroy();
+
+        // Initialize the main view
+        Ext.Viewport.add(Ext.create('Demo.view.Main'));
+    }
+});
---
+++ client/touch/app/view/Main.js
@@ -0,0 +1,10 @@
+Ext.define('Demo.view.Main', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'main',
+    requires: [
+
+    ],
+    config: {
+
+    }
+});
