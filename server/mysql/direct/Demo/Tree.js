07f4e8f00a33f820c49640aad8af55b40cfbfd70
Juris <juris@extjs.com>
2015-05-12 15:47:07 -0700

    Tweak tree response

--- server/mysql/direct/Demo/Tree.js
+++ server/mysql/direct/Demo/Tree.js
@@ -8,17 +8,30 @@ var Tree = {
             for(i = 1; i <= 5; ++i) {
                 out.push({
                     id: 'n'+ i,
-                    text: 'Node ' + i,
+                    text: 'Scenario ' + i,
+                    currentTest: 2,
+                    testStatus: 1,
+                    totalTests: 10,
+                    ignoredTests: 1,
                     leaf: false
 
                 });
             }
         } else if (params.node.length === 2) {
-            num = params.node.substr(0, 1);
+            //num = params.node.substr(0, 1);
             for(i = 1; i <= 5; ++i) {
                 out.push({
                     id: params.node + i,
-                    text: 'Node ' + num  +  '.'  + i,
+                    text: 'test-folder-' + i,
+                    leaf: false
+                });
+            }
+        } else if (params.node.length === 3) {
+            //num = params.node.substr(0, 1);
+            for(i = 1; i <= 5; ++i) {
+                out.push({
+                    id: params.node + i,
+                    text: 'test-file-' + i,
                     leaf: true
                 });
             }
