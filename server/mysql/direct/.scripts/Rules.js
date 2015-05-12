2f602bbbd3f588bbfd52d1a1e1dd9d51f7e66843
Juris <juris@extjs.com>
2015-05-11 18:38:28 -0700

    WIP on before / after action hooks

---
+++ server/mysql/direct/.scripts/Rules.js
@@ -0,0 +1,20 @@
+var Rules  = {
+
+    beforeAction: function(action, method, req, res, callback) {
+
+    },
+
+    afterAction: function(action, method, req, res, callback) {
+
+    },
+
+    beforeTransaction: function(action, method, req, res, callback) {
+
+    },
+
+    afterTransaction: function(action, method, req, res, callback) {
+
+    }
+};
+
+module.exports = Rules;
