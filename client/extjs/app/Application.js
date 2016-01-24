5609c370a007d4e784b07fa8531cfdaac84d7bd3
jgrocha <jgr@di.uminho.pt>
2016-01-24 23:45:09 +0000

    Loading Ext:direct api without loadScriptsSync blocker

--- client/extjs/app/Application.js
+++ client/extjs/app/Application.js
@@ -8,10 +8,6 @@ Ext.define('Demo.Application', {
 
     name: 'Demo',
 
-    requires:[
-        'Demo.DirectAPI'
-    ],
-
     views: [
         'MethodCall',
         'FormActions',
@@ -36,6 +32,26 @@ Ext.define('Demo.Application', {
     ],
 
     launch: function(){
+
+        var ns = Server.API;
+
+        /*
+         Add provider. Name must match settings on serverside
+         */
+
+        //Custom implementation. Works only with node backend and extdirect connector v2
+        //This feature is part ApiProcessor implementation
+        if(ns){
+            // Check for unexpected problems
+            // Node backend will set error object
+            if(ns.error){
+                //This is handled later in Application launch method
+                Demo.DirectError = ns.error;
+            } else {
+                Ext.direct.Manager.addProvider(ns);
+            }
+        }
+
         if(Demo.DirectError){
             Ext.Msg.alert('Error', Demo.DirectError.message);
         } else {
